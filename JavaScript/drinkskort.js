ad206f3d8027b938ca1ad50c1be56debe074f93c
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2024-01-12 18:34:41 +0100

    javascript

--- JavaScript/drinkskort.js
+++ JavaScript/drinkskort.js
@@ -1,29 +1,30 @@
 const froggysdrinksMap = {};
+
+const displayFirstThreeElements = window.location.href.includes('index.html');
 fetch('https://froggy-e3fac-default-rtdb.europe-west1.firebasedatabase.app/drinks.json')
     .then(response => response.json())
     .then(data => {
-        
         let contentContainer = document.getElementById("contentContainer");
         contentContainer.id = "overskrift-og-tekst";
         contentContainer.classList.add("drinkskort")
 
-        for (let key in data) {
-            let dataarray = data[key];
+        let count = 0; 
 
-            
+        for (let key in data) {
+            if (displayFirstThreeElements && count >= 3) {
+                break; 
+            }
+
+            let dataarray = data[key];
             let drinkElement = document.createElement("div");
             drinkElement.classList.add("drinksection");
-      
-            //drinkElement.textContent = `${dataarray.name} ${dataarray.price} kr. `;
 
             let nameElement = document.createElement("h3");
             nameElement.textContent = dataarray.name;
 
-            
             let priceElement = document.createElement("p");
             priceElement.textContent = `${dataarray.price} kr.`;
 
-            
             let imageElement = document.createElement("img")
             imageElement.src = dataarray.image;
             imageElement.classList.add("drinkskortbilleder");
@@ -31,8 +32,10 @@ fetch('https://froggy-e3fac-default-rtdb.europe-west1.firebasedatabase.app/drink
             drinkElement.appendChild(nameElement);
             drinkElement.appendChild(priceElement);
             drinkElement.appendChild(imageElement);
-             
+
             contentContainer.appendChild(drinkElement);
+
+            count++;
         }
     })
     .catch(error => console.error('Fejl:', error));
